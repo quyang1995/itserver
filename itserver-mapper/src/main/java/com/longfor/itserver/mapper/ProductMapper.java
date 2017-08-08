c9424a55697a3c47c18f2ec1a66e5f720962b17a
lilong <longli123>
2017-08-08 10:56:22 +0800

    lil

--- itserver-mapper/src/main/java/com/longfor/itserver/mapper/ProductMapper.java
+++ itserver-mapper/src/main/java/com/longfor/itserver/mapper/ProductMapper.java
@@ -3,6 +3,9 @@ package com.longfor.itserver.mapper;
 import com.longfor.itserver.entity.Product;
 import com.longfor.itserver.mapper.base.BeeMapper;
 
+import java.util.List;
+import java.util.Map;
+
 public interface ProductMapper extends BeeMapper<Product> {
     List<Product> selectList(Map map);
     List<Product> selectLikeList(Map map);
--- itserver-service/src/main/java/com/longfor/itserver/service/IProductService.java
+++ itserver-service/src/main/java/com/longfor/itserver/service/IProductService.java
@@ -4,6 +4,9 @@ package com.longfor.itserver.service;
 import com.longfor.itserver.entity.Product;
 import com.longfor.itserver.service.base.IAdminService;
 
+import java.util.List;
+import java.util.Map;
+
 public interface IProductService extends IAdminService<Product> {
     List<Product> searchList(Map map);
     List<Product> searchLikeList(Map map);
