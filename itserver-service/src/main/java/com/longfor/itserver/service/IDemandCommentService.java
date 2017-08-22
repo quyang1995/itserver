4efc2f2d57b0ae87d162d867330abd7e5d45a41d
wangsongkai <wangsongkai0821@rayootech.com>
2017-08-22 19:23:09 +0800

    查询评论

--- itserver-service/src/main/java/com/longfor/itserver/service/IDemandCommentService.java
+++ itserver-service/src/main/java/com/longfor/itserver/service/IDemandCommentService.java
@@ -3,7 +3,9 @@ package com.longfor.itserver.service;
 
 import com.longfor.itserver.entity.DemandComment;
 import com.longfor.itserver.service.base.IAdminService;
+import java.util.List;
 
 public interface IDemandCommentService extends IAdminService<DemandComment> {
 
+    List<DemandComment> getListById(DemandComment demandComment);
 }
--- itserver-service/src/main/java/com/longfor/itserver/service/impl/DemandCommentServiceImpl.java
+++ itserver-service/src/main/java/com/longfor/itserver/service/impl/DemandCommentServiceImpl.java
@@ -2,9 +2,13 @@ package com.longfor.itserver.service.impl;
 
 
 import com.longfor.itserver.entity.DemandComment;
+import com.longfor.itserver.mapper.DemandCommentMapper;
 import com.longfor.itserver.service.IDemandCommentService;
 import com.longfor.itserver.service.base.AdminBaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author wax
@@ -14,4 +18,16 @@ import org.springframework.stereotype.Service;
 @Service("DemandCommentService")
 public class DemandCommentServiceImpl extends AdminBaseService<DemandComment> implements IDemandCommentService {
 
+    @Autowired
+    DemandCommentMapper demandCommentMapper;
+
+
+
+    @Override
+    public List<DemandComment> getListById(DemandComment demandId) {
+
+        List<DemandComment> demandCommentList= demandCommentMapper.selectListById(demandId);
+
+        return demandCommentList;
+    }
 }
