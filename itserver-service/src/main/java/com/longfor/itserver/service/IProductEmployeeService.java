3690a9d15fb5ee2eae5dbd72a109708c23908b92
wangsongkai <wangsongkai0821@rayootech.com>
2017-09-04 18:08:16 +0800

    delEmployee

--- itserver-service/src/main/java/com/longfor/itserver/service/IProductEmployeeService.java
+++ itserver-service/src/main/java/com/longfor/itserver/service/IProductEmployeeService.java
@@ -5,11 +5,8 @@ import com.longfor.itserver.entity.ProductEmployee;
 import com.longfor.itserver.service.base.IAdminService;
 
 import java.util.List;
-import java.util.Map;
 
 public interface IProductEmployeeService extends IAdminService<ProductEmployee> {
     List<ProductEmployee> searchTypeList(Long productId, Integer employeeType, Long employeeTypeId);
 
-    boolean delEmployee (Map paramsMap);
-//    boolean delEmployee (HttpServletRequest  request);
 }
