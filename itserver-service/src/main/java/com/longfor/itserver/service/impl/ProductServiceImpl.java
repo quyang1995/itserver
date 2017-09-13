90d457ee3181c1464112ee4d300e37e5d1a1a8be
wangaixia <408563315@qq.com>
2017-09-13 18:08:26 +0800

    u

--- itserver-service/src/main/java/com/longfor/itserver/service/impl/ProductServiceImpl.java
+++ itserver-service/src/main/java/com/longfor/itserver/service/impl/ProductServiceImpl.java
@@ -341,7 +341,7 @@ public class ProductServiceImpl extends AdminBaseService<Product> implements IPr
 					.append(" 将 产品状态 从 [")
 					.append(ProductStatusEnum.getByCode(oldProduct.getStatus()).getText())
 					.append("] 更新为 [")
-					.append(ProductStatusEnum.getByCode((status)))
+					.append(ProductStatusEnum.getByCode((status)).getText())
 					.append("] ");
 		}
 		return  sb.toString();
