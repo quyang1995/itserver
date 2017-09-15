84dae0a2cf6a0cdab1cce2da8dad016d6c742332
wangsongkai <wangsongkai0821@rayootech.com>
2017-09-15 14:51:22 +0800

    statusList

--- itserver-service/src/main/java/com/longfor/itserver/service/impl/DemandServiceImpl.java
+++ itserver-service/src/main/java/com/longfor/itserver/service/impl/DemandServiceImpl.java
@@ -421,33 +421,6 @@ public class DemandServiceImpl extends AdminBaseService<Demand> implements IDema
 		PageHelper.startPage(elExample.getPageNum(), elExample.getPageSize(), true);
 
 		Map resultMap = CommonUtils.getResultMapByBizEnum(BizEnum.SSSS);
-//		JSONObject jsonObject = (JSONObject)JSONObject.toJSON(paramsMap);
-//		Demand demand = new Demand();
-//		String relationIds = jsonObject.getString("relationId");
-//		String relationTypes = jsonObject.getString("relationType");
-//		String accountId = jsonObject.getString("accountId");
-//		Integer  relationType = null;
-//		Long relationId = null;
-//		if(StringUtils.isNotBlank(relationTypes)){
-//			relationType = Integer.parseInt(relationTypes);
-//		}
-//		if(StringUtils.isNotBlank(relationIds)){
-//			relationId = Long.parseLong(relationIds);
-//		}
-//
-//		//关联产品
-//		if("1".equals(relationTypes)){
-//			demand.setRelationId(relationId);
-//			demand.setRelationType(relationType);
-//			demand.setCallonAccountId(accountId);
-//		}
-//		else if("2".equals(relationTypes)){
-//			//关联项目
-//			demand.setRelationId(relationId);
-//			demand.setRelationType(relationType);
-//			demand.setCallonAccountId(accountId);
-//		}
-//		List<Demand> list = demandMapper.statusList(demand);
 		List<Demand> list = demandMapper.statusList(paramsMap);
 		resultMap.put("list",list);
 		resultMap.put(APIHelper.PAGE_NUM, elExample.getPageNum());
