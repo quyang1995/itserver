e1303367c2da648985232eb04403105564309a4d
吴佳星 <wujiaxing@longfor.com>
2017-10-19 13:25:37 +0800

    loginName

--- itserver-service/src/main/java/com/longfor/itserver/service/util/AccountUitl.java
+++ itserver-service/src/main/java/com/longfor/itserver/service/util/AccountUitl.java
@@ -47,7 +47,7 @@ public class AccountUitl {
         if(buddyAccount==null)return null;
         accountLongfor = new AccountLongfor();
         accountLongfor.setName(buddyAccount.getName());
-        accountLongfor.setLoginName(buddyAccount.getLoginName());
+        accountLongfor.setLoginName(loginName);
         return accountLongfor;
     }
 }
