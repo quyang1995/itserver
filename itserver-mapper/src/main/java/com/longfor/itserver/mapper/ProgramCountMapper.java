48285f5a07df1984ad70a44f215f601a88a437d5
jitianxiang <jitianxiang111>
2018-01-19 18:35:26 +0800

    111

---
+++ itserver-mapper/src/main/java/com/longfor/itserver/mapper/ProgramCountMapper.java
@@ -0,0 +1,9 @@
+package com.longfor.itserver.mapper;
+
+
+import com.longfor.itserver.entity.ProgramCount;
+import com.longfor.itserver.mapper.base.BeeMapper;
+
+public interface ProgramCountMapper extends BeeMapper<ProgramCount> {
+
+}
